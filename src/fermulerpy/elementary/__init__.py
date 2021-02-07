629bcfb99404f8915c0151a1250b1a191b3dcdaa
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-02-07 19:43:29 +0530

    binds functions of elementary submodule

---
+++ src/fermulerpy/elementary/__init__.py
@@ -0,0 +1,17 @@
+from .integers import(
+    isDivisible,
+    gcd,
+    lcm,
+    division_algo_coeff,
+    isCoprime,
+    euclidean_algo
+)
+
+__all__ = [
+    "isDivisible",
+    "gcd",
+    "lcm",
+    "division_algo_coeff",
+    "isCoprime",
+    "euclidean_algo",
+]
