26eeba1bf7674b276305322022e3ef91d02103e4
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-02-13 16:55:36 +0530

    adds euler functions

--- src/fermulerpy/elementary/euler.py
+++ src/fermulerpy/elementary/euler.py
@@ -1,4 +1,6 @@
-from fermulerpy.elementary import prime_divisors
+import warnings
+
+from fermulerpy.elementary import (prime_divisors , gcd)
 
 def euler_function(n):
     """
@@ -12,6 +14,10 @@ def euler_function(n):
         returns the value of φ(n)
 
     """
+    if(n < 2):
+        raise ValueError(
+            "Input must be positive integer greater than 1"
+        )
     prime_divisors_arr = prime_divisors(n)
 
     num = 1
@@ -23,4 +29,28 @@ def euler_function(n):
         den *= i
     
     return int((num * n)/den)
+
+def classes(n):
+    """
+    Returns different classes for a positive integer n.
+    Two integers are in same class only and only if they have same gcd with n.
+
+    Parameters
+    ----------
+    n : int
+        denotes positive integer of which class distribution is neede
+    return : dictionary
+        key : int
+             denotes the gcd values
+        value : array
+             denotes the elements belonging to same class
+
+    """
+    classes_dict = {}
+    for i in range(1,n+1):
+        if gcd(i,n) in classes_dict:
+            classes_dict[gcd(i,n)].append(i)
+        else:
+            classes_dict[gcd(i,n)] = [i]
+    return classes_dict
 
