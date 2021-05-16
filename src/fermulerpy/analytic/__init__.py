6f3ba4e665569584ca28e9722b68d021f8d60072
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-05-16 14:44:55 +0530

    adds function names in init.py

--- src/fermulerpy/analytic/__init__.py
+++ src/fermulerpy/analytic/__init__.py
@@ -2,6 +2,84 @@ from .arithmetic_fundamental import(
     gcd,
 )
 
+from .arithmetic_periodic import(
+    fourier_coeff,
+    is_gauss_sum_separable,
+)
+
+from .arithmetical_functions import(
+    mobius,
+    euler_totient,
+    mangoldt,
+    totient_convolution,
+    mangoldt_convolution,
+    liouville,
+)
+
+from .dirichlet import(
+    if_infinite_prime_AP,
+    prime_count_AP,
+    dirichlet_function,
+)
+
+from .function_average import(
+    divisor_function_avg,
+    euler_totient_avg,
+    mobius_avg,
+    liouville_avg,
+)
+
+from .partitions import(
+    euler_pentagonal_num,
+    partition_unique,
+)
+
+from .prime_distribution import(
+    pi_asymptotic,
+    chebyshev_chix,
+    chebyshev_chix_asymptotic,
+)
+
+from .quadratic_residue import(
+    quadratic_residues,
+    legendre_euler_symbol,
+    jacobi_symbol,
+)
+
+from .riemann import(
+    zeta_function,
+    altzeta_function,
+    gamma_function,
+    bernoulli_num,
+)
+
 __all__ = [
     "gcd",
+    "fourier_coeff",
+    "is_gauss_sum_separable",
+    "mobius",
+    "euler_totient",
+    "mangoldt",
+    "totient_convolution",
+    "mangoldt_convolution",
+    "liouville",
+    "if_infinite_prime_AP",
+    "prime_count_AP",
+    "dirichlet_function",
+    "divisor_function_avg",
+    "euler_totient_avg",
+    "mobius_avg",
+    "liouville_avg",
+    "euler_pentagonal_num",
+    "partition_unique",
+    "pi_asymptotic",
+    "chebyshev_chix",
+    "chebyshev_chix_asymptotic",
+    "quadratic_residues",
+    "legendre_euler_symbol",
+    "jacobi_symbol",
+    "zeta_function",
+    "altzeta_function",
+    "gamma_function",
+    "bernoulli_num",
 ]
