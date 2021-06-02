69b0d91246c4e146c730547493378658cb4ebd1d
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-06-02 14:39:39 +0530

    write tests for euler

---
+++ tests/test_elementary/test_euler.py
@@ -0,0 +1,15 @@
+import pytest
+
+from fermulerpy.elementary import euler_function, classes
+
+def test_euler_function1():
+    assert euler_function(10) == 4
+
+def test_euler_function2():
+    assert euler_function(9) == 6
+
+def test_classes1():
+    assert classes(10) == {1: [1, 3, 7, 9], 2: [2, 4, 6, 8], 5: [5], 10: [10]}
+
+def test_classes2():
+    assert classes(15) == {1: [1, 2, 4, 7, 8, 11, 13, 14], 3: [3, 6, 9, 12], 5: [5, 10], 15: [15]}
